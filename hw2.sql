a7440dc39ac0b2040737538be96fe252ada67a73
Fritz <fjbeimesch@fuse.net>
2018-09-11 18:49:07 -0400

    update releasedate constraint

--- hw2.sql
+++ hw2.sql
@@ -14,7 +14,7 @@ create table Movies
 		"Title" varchar2(250) Primary key,
 		"Rating" decimal(2) check ("Rating">0 AND "Rating"<11),
 		"Length" number(3) check ("Length">0),
-		"ReleaseDate" varchar2(50) check ("ReleaseDate" > '1990-01-01') -- might be wrong --
+		"ReleaseDate" varchar2(50) check (to_date(concat(concat(concat(trim(SUBSTR(Movies."ReleaseDate", 1,2)), '-'), concat(trim(SUBSTR(Movies."ReleaseDate", 3,3)), '-')), trim(SUBSTR(Movies."ReleaseDate", length(Movies."ReleaseDate")-4, length(Movies."ReleaseDate"))))) > '01-JAN-1990') -- might be wrong --
 	)
 ;
 
@@ -25,3 +25,13 @@ create table ShownAt
     Primary key ("TheatreName", "MovieTitle")
 	)
 ;
+
+insert into Movies values ('Big Hero 6', 8.5, 102, '7 November 2014');
+
+select
+  SUBSTR(Movies."ReleaseDate", 1,2) as myDay,
+  SUBSTR(Movies."ReleaseDate", 3,3) as myMonth,
+  SUBSTR(Movies."ReleaseDate", length(Movies."ReleaseDate")-4, length(Movies."ReleaseDate")) as myYear,
+  to_date(concat(concat(concat(trim(SUBSTR(Movies."ReleaseDate", 1,2)), '-'), concat(trim(SUBSTR(Movies."ReleaseDate", 3,3)), '-')), trim(SUBSTR(Movies."ReleaseDate", length(Movies."ReleaseDate")-4, length(Movies."ReleaseDate")))))  as myDate
+  from Movies
+  ;
